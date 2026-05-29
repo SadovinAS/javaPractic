4e258b118d045616d327afe47048b48f5c047177
SadovinAS <sadovin.as@edu.spbstu.ru>
2026-05-29 19:48:45 +0300

    practic

---
+++ Date.java
@@ -0,0 +1,8 @@
+public class Date {
+    Day day;
+    String event;
+    Date(Day day, String event){
+        this.day = day;
+        this.event = event;
+    }
+}
--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-
+        Date D = new Date(Day.MONDAY, "New week");
     }
 }
