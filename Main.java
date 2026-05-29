307c43d2a9897d650d453d0369ad70e3336ee6f2
SadovinAS <sadovin.as@edu.spbstu.ru>
2026-05-29 21:52:20 +0300

    practic

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
-
 public class Main {
     public static void main(String[] args) {
-
+        String s = null;
+        try {
+            System.out.println(s.length());
+        } catch (NullPointerException e) {
+            System.out.println("Указатель - null");
+        }
     }
 }
