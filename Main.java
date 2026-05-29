511da05ecfdaa0209608ac0ee72f116cad904ae7
SadovinAS <sadovin.as@edu.spbstu.ru>
2026-05-29 21:01:33 +0300

    practic

--- Main.java
+++ Main.java
@@ -1,6 +1,17 @@
+interface Math {
+    double oper(double a, double b);
 
+    class Add implements Math {
+        @Override
+        public double oper(double a, double b) {
+            return a + b;
+        }
+    }
+}
 public class Main {
     public static void main(String[] args) {
+        Math adder = new Math.Add();
 
+        System.out.println(adder.oper(5, 3));
     }
 }
