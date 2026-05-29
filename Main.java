9d15a2ac8ef75e1957e0e8dcb810fd161a6c637f
SadovinAS <sadovin.as@edu.spbstu.ru>
2026-05-29 21:20:34 +0300

    practic

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
-
 public class Main {
     public static void main(String[] args) {
-
+        java.util.StringJoiner joiner = new java.util.StringJoiner("->", "[", "]");
+        joiner.add("A");
+        joiner.add("B");
+        joiner.add("C");
+        System.out.println(joiner);
     }
 }
