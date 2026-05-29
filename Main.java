ff9f637f8cfb787abbc5dc3a4d3e7799047831bd
SadovinAS <sadovin.as@edu.spbstu.ru>
2026-05-29 19:11:26 +0300

    practic

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int i = (a == 1 ? 1 : (a == 2 ? 2 : 3));
+        System.out.println(i);
     }
 }
