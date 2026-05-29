2c47b58e3b4f85c201b67c277bc3d6e8c24c9795
SadovinAS <sadovin.as@edu.spbstu.ru>
2026-05-29 19:51:34 +0300

    practic

--- Main.java
+++ Main.java
@@ -1,6 +1,21 @@
-
+enum Operation {
+    ADD {
+        int oper(int a, int b) {
+            return a + b;
+        }
+    },
+    SUB {
+        int oper(int a, int b) {
+            return a - b;
+        }
+    };
+    abstract int oper(int a, int b);
+}
 public class Main {
     public static void main(String[] args) {
-
+        Operation op = Operation.ADD;
+        System.out.println(op.oper(1, 4));
+        op = Operation.SUB;
+        System.out.println(op.oper(1, 4));
     }
 }
