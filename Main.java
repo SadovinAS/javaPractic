a0b8ae051e35038960efb0bf98bbe2c3ce47ae6b
SadovinAS <sadovin.as@edu.spbstu.ru>
2026-05-29 21:07:26 +0300

    practic

--- Main.java
+++ Main.java
@@ -1,6 +1,18 @@
-
+interface A {
+    default void print() {
+        System.out.println("A");
+    }
+}
+interface B {
+    default void print() {
+        System.out.println("B");
+    }
+}
+class C implements A, B {
+}
 public class Main {
     public static void main(String[] args) {
-
+        C t = new C();
+        t.print();
     }
 }
