3a46b07003b9d695682cea16077e8f6c2b573a14
SadovinAS <sadovin.as@edu.spbstu.ru>
2026-05-29 20:04:15 +0300

    practic

--- Main.java
+++ Main.java
@@ -1,6 +1,19 @@
-
+class A {
+    public void print( int a ) {
+        System.out.println(a);
+    }
+    public void print( double a ) {
+        System.out.println(a);
+    }
+    public void print( char a ) {
+        System.out.println(a);
+    }
+}
 public class Main {
     public static void main(String[] args) {
-
+        A a = new A();
+        a.print(1);
+        a.print(2.0 / 3);
+        a.print('f');
     }
 }
