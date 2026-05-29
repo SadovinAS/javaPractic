93ff29825f6fd1574c4191068193ff4ff6b84a66
SadovinAS <sadovin.as@edu.spbstu.ru>
2026-05-29 21:51:05 +0300

    practic

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
-
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            String A = String.format("%d", "hello");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Неверный Формат");
+        }
     }
 }
