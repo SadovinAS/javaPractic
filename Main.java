2dc9256c240f751367c2c63e4ee88dcf82a542b7
SadovinAS <sadovin.as@edu.spbstu.ru>
2026-05-29 20:56:39 +0300

    practic

--- Main.java
+++ Main.java
@@ -1,6 +1,14 @@
 
-public class Main {
-    public static void main(String[] args) {
 
+class Main {
+    public static void main(String[] args) {
+        Outer outer = new Outer();
+
+        // Outer.InnerPrivate ip = outer.new InnerPrivate(); // не доступно
+        Outer.InnerDefault id = outer.new InnerDefault();    // доступно в том же пакете
+        Outer.InnerPublic ipu = outer.new InnerPublic();     //доступно
+
+        id.show();
+        ipu.show();
     }
 }
