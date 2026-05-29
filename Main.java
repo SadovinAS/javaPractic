93addc6f421c9d1b418a4659ab9a0b58cef6c8b6
SadovinAS <sadovin.as@edu.spbstu.ru>
2026-05-29 18:53:41 +0300

    practic 3_6

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public class Main {
         double a = 15.67;
         byte b = 12;
         int c = 2000000000;
+
         System.out.println((int)a);
         System.out.println((float)b);
         System.out.println((double)c);
