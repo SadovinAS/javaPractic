6503eb713df8408d8edf657d5315e036fbff834f
SadovinAS <sadovin.as@edu.spbstu.ru>
2026-05-29 22:02:23 +0300

    practic

--- Main.java
+++ Main.java
@@ -1,6 +1,16 @@
-
+class My implements AutoCloseable {
+    public void hello() {
+        System.out.println("Работа");
+    }
+    @Override
+    public void close() {
+        System.out.println("Закрытие");
+    }
+}
 public class Main {
     public static void main(String[] args) {
-
+        try (My r = new My()) {
+            r.hello();
+        }
     }
 }
