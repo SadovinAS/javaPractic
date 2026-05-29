e1975cb44e2bb9af23168c8d5dee0bc7abd98748
SadovinAS <sadovin.as@edu.spbstu.ru>
2026-05-29 21:56:16 +0300

    practic

--- Main.java
+++ Main.java
@@ -1,6 +1,19 @@
-
+class Ex1 extends Exception { }
+class Ex2 extends Ex1 { }
+class Ex3 extends Ex2 { }
 public class Main {
+    static void test() throws Ex3 {
+        throw new Ex3();
+    }
     public static void main(String[] args) {
-
+        try {
+            test();
+        } catch (Ex3 e) {
+            System.out.println("Ex3");
+        } catch (Ex2 e) {
+            System.out.println("Ex2");
+        } catch (Ex1 e) {
+            System.out.println("Ex1");
+        }
     }
 }
