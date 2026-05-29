67e9987a56cc8823096771aa9b45a6a61fc2d242
SadovinAS <sadovin.as@edu.spbstu.ru>
2026-05-29 19:39:20 +0300

    practic

--- Main.java
+++ Main.java
@@ -1,6 +1,15 @@
-
+class Box<T> {
+    T value;
+    Box(T value) {
+        this.value = value;
+    }
+}
 public class Main {
     public static void main(String[] args) {
+        Box<String> box = new Box<>("Hello");
 
+        if (box instanceof Box<?>) {
+            System.out.println("yes");
+        }
     }
 }
