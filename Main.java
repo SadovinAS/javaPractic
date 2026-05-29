ee8291ac27b4a72f72a01b515301b886e154fd24
SadovinAS <sadovin.as@edu.spbstu.ru>
2026-05-29 18:10:19 +0300

    Practic 3_1

--- Main.java
+++ Main.java
@@ -1,32 +1,12 @@
-abstract class Figure {
-    public abstract double getVolume();
-}
-class Sphere extends Figure {
-    private double R;
-    public Sphere(double rad) {
-        this.R = rad;
-    }
-    @Override
-    public double getVolume() {
-        return 4.0 / 3 * 3.14159365 * R * R * R;
-    }
-}
-class Cube extends Figure {
-    private double A;
-    public Cube(double len) {
-        this.A = len;
-    }
-    @Override
-    public double getVolume() {
-        return A * A * A;
-    }
-}
 public class Main {
     public static void main(String[] args) {
-        Figure f1 = new Sphere(2.5);
-        Figure f2 = new Cube(5);
-
-        System.out.println(f1.getVolume());
-        System.out.println(f2.getVolume());
+        byte a = 111;
+        short b = 12345;
+        int c = 876523;
+        long d = -245674329;
+        float e = 0;
+        double f = 0.000000001;
+        char g = 'a';
+        boolean h = false;
     }
 }
