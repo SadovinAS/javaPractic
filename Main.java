2e99f8c25c1793454ebe689e2e02358d5e6ec754
SadovinAS <sadovin.as@edu.spbstu.ru>
2026-05-29 21:57:28 +0300

    practic

--- Main.java
+++ Main.java
@@ -1,6 +1,20 @@
-
+class MyFirstException extends Exception {
+    public MyFirstException(String message) {
+        super(message);
+    }
+}
 public class Main {
+    static double sqrt(double x) throws MyFirstException {
+        if (x < 0) {
+            throw new MyFirstException("Число меньше нуля");
+        }
+        return Math.sqrt(x);
+    }
     public static void main(String[] args) {
-
+        try {
+            System.out.println(sqrt(-5));
+        } catch (MyFirstException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
