00e195d3f131251cbff6cbc3a843721128c06c1f
SadovinAS <sadovin.as@edu.spbstu.ru>
2026-05-29 21:46:45 +0300

    practic

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
-
+import java.io.PrintWriter;
 public class Main {
     public static void main(String[] args) {
-
+        PrintWriter pw = new PrintWriter(System.out);
+        pw.println("Hello");
+        pw.flush();
     }
 }
