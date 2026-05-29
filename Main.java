d64e9160f3754c5d7deee2c4df460755f0957407
SadovinAS <sadovin.as@edu.spbstu.ru>
2026-05-29 21:47:14 +0300

    practic

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
-
+import java.util.Date;
 public class Main {
     public static void main(String[] args) {
-
+        Date date = new Date();
+        System.out.printf("%tH%n", date); // часы
+        System.out.printf("%tM%n", date); // минуты
+        System.out.printf("%tS%n", date); // секунды
+        System.out.printf("%tY%n", date); // год
+        System.out.printf("%tB%n", date); // месяц
     }
 }
