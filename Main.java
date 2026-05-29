855bf2de59ad425ec3a011e445a37b60f1a5555c
SadovinAS <sadovin.as@edu.spbstu.ru>
2026-05-29 19:16:29 +0300

    practic

--- Main.java
+++ Main.java
@@ -1,6 +1,14 @@
 
 public class Main {
     public static void main(String[] args) {
-
+        int k = 0;
+        while (k != 100){
+            k++;
+        }
+        System.out.println(k);
+        do {
+            k--;
+        } while(k != 0);
+        System.out.println(k);
     }
 }
