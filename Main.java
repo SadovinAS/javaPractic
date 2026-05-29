700caa22fb89670043e01b0e1e02e28c64c06a07
SadovinAS <sadovin.as@edu.spbstu.ru>
2026-05-29 22:07:42 +0300

    practic

--- Main.java
+++ Main.java
@@ -1,6 +1,15 @@
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("test.txt"));
+        String line;
+        while(sc.hasNext())
+        {
+            line = sc.next();
+            System.out.println(line);
+        }
+        sc.close();
     }
 }
