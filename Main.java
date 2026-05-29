27bbbd88a55ab15cb9d731453127b7dfdbdba173
SadovinAS <sadovin.as@edu.spbstu.ru>
2026-05-29 18:59:22 +0300

    practic

--- Main.java
+++ Main.java
@@ -1,6 +1,17 @@
-
 public class Main {
+    /**
+     * @author - Садовин Александр
+     * @version 1.0.0
+     * @param a Первое Слагаемое
+     * @param b Второе Слагаемое
+     * @return Сумма Слагаемых a и b
+     */
+    public static int sum(int a , int b) {
+        return a + b;
+    }
     public static void main(String[] args) {
-
+        int a = 5;
+        int b = 3;
+        System.out.println(sum(a, b));
     }
 }
