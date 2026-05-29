b972f01e95f5c1827c7b1122e6ce4f6f8cff3831
SadovinAS <sadovin.as@edu.spbstu.ru>
2026-05-29 19:05:38 +0300

    practic

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
-
 public class Main {
     public static void main(String[] args) {
-
+        Integer T1 = Integer.decode("12"); // T1= 12
+        Integer T2 = Integer.decode("-12"); // T2= -12
+        Integer T3 = Integer.decode("012"); // T3= 10
+        Integer T4 = Integer.decode("0x12"); // T4= 18
     }
 }
