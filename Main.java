d0cf40ef110f0fd42bd2bf6fb9fad808e713a217
SadovinAS <sadovin.as@edu.spbstu.ru>
2026-05-29 19:45:36 +0300

    practic

--- Main.java
+++ Main.java
@@ -1,6 +1,19 @@
+class Sum{
+    public static int sum(int a, int b){
+        return a + b;
+    }
+    public static int sum(int... nums){
+        int sum = 0;
+        for(int n : nums){
+            sum += n;
+        }
+        return sum;
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(Sum.sum(5, 4));
+        System.out.println(Sum.sum(5, 4, 3, 2, 1));
     }
 }
