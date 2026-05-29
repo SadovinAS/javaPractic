8ee3955f35cd523972eb7df64b70887211d472ea
SadovinAS <sadovin.as@edu.spbstu.ru>
2026-05-29 21:55:48 +0300

    practic

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
-
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            int[] arr = {1, 2, 3};
+            int x = 10 / 0;
+            System.out.println(arr[3]);
+        } catch (ArithmeticException | ArrayIndexOutOfBoundsException e) {
+            System.out.println("Ошибка");
+        }
     }
 }
