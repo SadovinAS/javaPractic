444dffbe4127961b3b6456578770f62bd48324a5
SadovinAS <sadovin.as@edu.spbstu.ru>
2026-05-29 20:05:48 +0300

    practic

--- Main.java
+++ Main.java
@@ -1,6 +1,16 @@
-
+class A {
+    public void print( int a ) {
+        System.out.println(a);
+    }
+}
+class B extends A {
+    public void print(int a) {
+        System.out.println("B -> " + a);
+    }
+}
 public class Main {
     public static void main(String[] args) {
-
+        B b = new B();
+        b.print(1);
     }
 }
