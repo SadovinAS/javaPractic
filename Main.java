b87c3bbf92056f37a2cc819ebd07952369317d58
SadovinAS <sadovin.as@edu.spbstu.ru>
2026-05-29 21:43:27 +0300

    practic

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
-
 public class Main {
     public static void main(String[] args) {
-
+        System.out.printf("%b%n", true);        // boolean
+        System.out.printf("%c%n", 'A');         // char
+        System.out.printf("%d%n", 123);         // int
+        System.out.printf("%f%n", 12.3456);     // float/double
+        System.out.printf("%s%n", "Hello");     // String
     }
 }
