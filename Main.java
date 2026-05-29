0d102b40243d6dd44d669bbb9a1bfbd50e2cb970
SadovinAS <sadovin.as@edu.spbstu.ru>
2026-05-29 22:01:08 +0300

    practic

--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
-
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        OutputStream out = new FileOutputStream("out.txt");
+        out.write(65);
+        out.write(66);
+        out.write(67);
+        out.close();
     }
 }
