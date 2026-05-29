6904fbda72c7335d329ce8c7531fd00cbc18c615
SadovinAS <sadovin.as@edu.spbstu.ru>
2026-05-29 21:23:18 +0300

    practic

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
-
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("""
+        int x = 10;
+        int y = 5;
+        System.out.println(x + y);
+        """);
     }
 }
