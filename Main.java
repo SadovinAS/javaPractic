3a9384b8823fd4a66d50e5abe1cf75202b8534cb
SadovinAS <sadovin.as@edu.spbstu.ru>
2026-05-29 19:06:20 +0300

    practic

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
 
 public class Main {
     public static void main(String[] args) {
-
+        Boolean b1 = Boolean.valueOf(true);
+        Boolean b2 = Boolean.valueOf("true");
+        Boolean b3 = Boolean.TRUE;
+        Boolean b4 = Boolean.FALSE;
+        Boolean b5 = true;
+        Boolean b6 = false;
     }
 }
