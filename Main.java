9ea15d6bee916e9e6eaed3852cd1eb198ce6459a
SadovinAS <sadovin.as@edu.spbstu.ru>
2026-05-29 21:59:15 +0300

    practic

--- Main.java
+++ Main.java
@@ -1,6 +1,13 @@
-
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        byte[] data = {65, 66, 67};
+        InputStream in = new ByteArrayInputStream(data);
+        int x;
+        while ((x = in.read()) != -1)
+            System.out.println((char)x);
+        in.close();
     }
 }
