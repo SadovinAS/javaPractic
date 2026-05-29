e24e909a9270ade0075dd3d376bcc0fc98e22835
SadovinAS <sadovin.as@edu.spbstu.ru>
2026-05-29 19:18:34 +0300

    practic

--- Main.java
+++ Main.java
@@ -1,6 +1,17 @@
 
 public class Main {
     public static void main(String[] args) {
-
+        int i = 0;
+        int sum = 0;
+        while(true)
+        {
+            i = i + 1;
+            if(i % 5 == 0)
+                continue; // пропускает числа, делящиеся на 5
+            sum = sum + i;
+            if(i == 101)
+                break;
+        }
+        System.out.println(sum);
     }
 }
