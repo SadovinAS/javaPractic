83dcc61facbbdf062c6d1a4e65237ec50f7d3028
SadovinAS <sadovin.as@edu.spbstu.ru>
2026-05-29 21:51:44 +0300

    practic

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
-
 public class Main {
     public static void main(String[] args) {
-
+        Object obj = "Hello";
+        try {
+            Integer x = (Integer) obj;
+        } catch (ClassCastException e) {
+            System.out.println("Неверное приведение типов");
+        }
     }
 }
