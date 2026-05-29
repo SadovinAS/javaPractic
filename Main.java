6275c1325457366ca43fc410833b2ce051cb37b8
SadovinAS <sadovin.as@edu.spbstu.ru>
2026-05-29 21:05:56 +0300

    practic

--- Main.java
+++ Main.java
@@ -1,6 +1,17 @@
-
+interface Math {
+    default int add(int x, int y) {
+        return x + y;
+    }
+    static int sub(int x, int y) {
+        return x - y;
+    }
+}
+class Calculator implements Math {
+}
 public class Main {
     public static void main(String[] args) {
-
+        Calculator c = new Calculator();
+        System.out.println(c.add(5, 4)); // Обращение через Calculator c
+        System.out.println(Math.sub(3, 2)); // Обращение через Интерфейс
     }
 }
