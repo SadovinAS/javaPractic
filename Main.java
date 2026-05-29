4676f0daddeb565d90e646d044afaee693b100fd
SadovinAS <sadovin.as@edu.spbstu.ru>
2026-05-29 19:55:48 +0300

    practic

--- Main.java
+++ Main.java
@@ -1,6 +1,15 @@
+final class Add {
+    public int oper1(int a, int b) {
+        return a + b;
+    }
+}
 
+class Sub extends Add {
+    public int oper2(int a, int b) {
+        return a - b;
+    }
+}
 public class Main {
     public static void main(String[] args) {
-
     }
 }
